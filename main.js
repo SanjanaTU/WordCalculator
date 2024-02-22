4c27f8a69d6addc46af7a9dc14a95f0abef1db2d
Sanjana <sanjana.js02@gmail.com>
2024-02-22 07:17:10 +0100

    done

---
+++ main.js
@@ -0,0 +1,8 @@
+function calculate(){
+    let words = document.getElementById("input").value;
+    let wordCount = words.length;
+    let result = document.getElementById("result");
+    result.innerHTML = wordCount + "Number of words"
+   
+
+}
